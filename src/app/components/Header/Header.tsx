0575a0f7972debd1548389a4f8b6c5ab975db953
nickhwite <bbyicnka@yandex.ru>
2024-03-05 14:33:56 +0300

    feat(header&footer): Поправил header & footer

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react'
 
+import { Icon } from '@/app/components/Icon/Icon'
+
 import { Label } from '../Label'
 import { Links } from '../Links'
 import { Button } from '../Button'
@@ -10,8 +12,13 @@ export const Header: FC = () => (
   <header className={s.header}>
     <div className={s.header__wrapper}>
       <div className={s.menu}>
+        <button className={s.btn}>
+          <Icon name="menu" />
+        </button>
         <Label />
-        <Links background="tertiary" />
+        <div className={s.header__links}>
+          <Links background="tertiary" />
+        </div>
         <Button text="Перейти в телеграм" />
       </div>
     </div>
