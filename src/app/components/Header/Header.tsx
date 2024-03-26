90d495dacac63199b9f1c95f4f6902b53574e8dd
nickhwite <bbyicnka@yandex.ru>
2024-03-26 18:16:09 +0300

    fix(new-faq): Поправил faq

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,25 +1,40 @@
-import { FC } from 'react'
+'use client'
 
-import s from './Header.module.css'
+import { FC, useState } from 'react'
 
+import { MenuModal } from "@/app/components/MenuModal"
+
+import { Button } from "../Button"
 import { Icon } from '../Icon'
 import { Label } from '../Label'
 import { Links } from '../Links'
-import { Button } from '../Button'
 
-export const Header: FC = () => (
-  <header className={s.header}>
-    <div className={s.header__wrapper}>
-      <div className={s.menu}>
-        <button className={s.btn}>
-          <Icon name="menu" />
-        </button>
-        <Label />
-        <div className={s.header__links}>
-          <Links background="tertiary" />
+import s from './Header.module.css'
+
+export const Header: FC = () => {
+  const [modal, setActiveModal] = useState(false)
+  const closeModal = () => {
+    setActiveModal(false)
+  }
+  const activeModal = () => {
+    setActiveModal(true)
+  }
+
+  return (
+    <header className={s.header}>
+      <div className={s.header__wrapper}>
+        <div className={s.menu}>
+          <button className={s.btn} onClick={activeModal}>
+            <Icon name="menu" />
+          </button>
+          <Label />
+          <div className={s.header__links}>
+            <Links background="tertiary" />
+          </div>
+          <Button text="Перейти в телеграм" />
         </div>
-        <Button text="Перейти в телеграм" />
       </div>
-    </div>
-  </header>
-)
+      <MenuModal closeModal={closeModal} modal={modal} />
+    </header>
+  )
+}
