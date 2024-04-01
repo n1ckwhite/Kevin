53a0ef71c3374af7f2ec5f3f31421197e2412c21
nickhwite <bbyicnka@yandex.ru>
2024-04-01 14:11:26 +0300

    feat(kevin): делаю kevin блок

---
+++ src/app/components/Kevin/Kevin.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react"
+
+import { KevinMessage } from "./components/KevinMessage"
+import { KevinTitle } from "./components/KevinTitle"
+import s from './Kevin.module.css'
+
+export const Kevin: FC = () => (
+  <div className={s.wrapper}>
+    <div className={s.wrapper__scroll}>
+      <KevinTitle />
+      <KevinMessage />
+    </div>
+
+  </div>
+)
